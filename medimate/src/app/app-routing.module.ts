f2fe578a8b4428ac7d59d17b56db59a7b2545a27
Emerson Clay <136268542+c1ayer@users.noreply.github.com>
2024-12-11 17:43:11 -0600

    Few things
    Reminder component, worked on the delete, scss updates

--- medimate/src/app/app-routing.module.ts
+++ medimate/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LocationsComponent } from './locations/locations.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RemindersComponent } from './reminders/reminders.component';
 
 const routes: Routes = [
   {path: 'home', component:HomeComponent},
   {path: 'locations',component:LocationsComponent},
-  {path:'', redirectTo:'home',pathMatch:'full'}
+  {path:'', redirectTo:'home',pathMatch:'full'},
+  { path: 'reminder', component:RemindersComponent}, // New route
 ];
 
 @NgModule({
