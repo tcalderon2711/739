39ba03e48ee8ef6eb7703b9a17d11c1d34b5ac1e
tcalderon2711 <121997280+tcalderon2711@users.noreply.github.com>
2024-12-13 17:43:14 -0600

    clean up

--- backendRX/src/fda.ts
+++ backendRX/src/fda.ts
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios=require('axios');
+const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
@@ -7,36 +7,32 @@ app.use(cors());
 
 
 app.get('/api/generic-name', async (req, res) => {
-    const brandName = req.query.brand_name;
-    console.log('Received brand name: ', brandName);
-    const url = `https://api.fda.gov/drug/drugsfda.json?search=openfda.brand_name:"${brandName}"`;
-  
-    try {
-      const response = await axios.get(url);
+  const brandName = req.query.brand_name;
+  const url = `https://api.fda.gov/drug/drugsfda.json?search=openfda.brand_name:"${brandName}"`;
 
-      const results=response.data.results;
-      if(!results||results.length===0){
-        return res.status(404).json({error:'No products found'});
-      }
-      const genericNames=results.flatMap((result) => {
-        const genericNamesArray=result.openfda?.generic_name|| [];
-      return Array.isArray(genericNamesArray) ? genericNamesArray:[];
-      });
+  try {
+    const response = await axios.get(url);
 
-      const uniqueGenericNames=[...new Set(genericNames)];
-
-      console.log('Generic Names',uniqueGenericNames);
-
-      if(uniqueGenericNames.length===0){
-        return res.status(404).json({error:"No generic names"});
-      }
-
-      res.json(uniqueGenericNames);
-    } catch (error) {
-      //console.error(error.message);
-      res.status(500).json({ error: 'Error fetching generic names from openFDA.' });
+    const results = response.data.results;
+    if (!results || results.length === 0) {
+      return res.status(404).json({ error: 'No products found' });
     }
-  });
+    const genericNames = results.flatMap((result) => {
+      const genericNamesArray = result.openfda?.generic_name || [];
+      return Array.isArray(genericNamesArray) ? genericNamesArray : [];
+    });
+
+    const uniqueGenericNames = [...new Set(genericNames)];
+
+    if (uniqueGenericNames.length === 0) {
+      return res.status(404).json({ error: "No generic names" });
+    }
+
+    res.json(uniqueGenericNames);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching generic names from openFDA.' });
+  }
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
--- backendRX/src/index.ts
+++ backendRX/src/index.ts
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors=require("cors");
+const cors = require("cors");
 const port = 3000;
 
 const app = express();
@@ -9,9 +9,7 @@ const fs = require("fs");
 const filePath = path.join(__dirname, "rx.json");
 
 app.use(cors());
-// const corsOptions={
-//     origin:['http://localhost:4200']
-// }
+
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
@@ -28,66 +26,66 @@ fs.readFile(filePath, (err: any, data: any) => {
     }
 });
 
-app.get("/", (req, resp) => {    
+app.get("/", (req, resp) => {
     resp.status(200);
-    return resp.json(rxList);       
+    return resp.json(rxList);
 });
 
 app.get("/:name", (req, resp) => {
     const prescription = rxList.find(c => c.name == req.params.name);
-    
+
     if (prescription) {
         resp.status(200);
         return resp.json(prescription);
     }
 
     resp.status(404);
-    return resp.json({error: `Prescription: ${req.params.name} not found.`});
+    return resp.json({ error: `Prescription: ${req.params.name} not found.` });
 });
 
 app.post("/", (req, resp) => {
     // console.log(`body is ${JSON.stringify(req.body)}`);
     // return resp.sendStatus(200);
-    const {name, dose,unit,frequency}=req.body;
+    const { name, dose, unit, frequency } = req.body;
     if (name && dose && unit && frequency) {
-        const newEntry={name, dose,unit,frequency }
+        const newEntry = { name, dose, unit, frequency }
         rxList.push(newEntry);
         resp.status(200);
         return resp.json(newEntry);
     }
 
     resp.status(404);
-    return resp.json({error: `Prescription with name: ${req.params.name} not found.`});
+    return resp.json({ error: `Prescription with name: ${req.params.name} not found.` });
 });
 
-app.put("/:name",(req,resp)=>{
+app.put("/:name", (req, resp) => {
     const prescription = rxList.find(c => c.name == req.params.name);
-    
+
     if (prescription) {
-        const {name, dose, unit, frequency}=req.body;
-        prescription.name=name;
-        prescription.dose=dose;
-        prescription.unit=unit;
-        prescription.frequency=frequency;
+        const { name, dose, unit, frequency } = req.body;
+        prescription.name = name;
+        prescription.dose = dose;
+        prescription.unit = unit;
+        prescription.frequency = frequency;
         resp.status(200);
         return resp.json(prescription);
     }
 
     resp.status(404);
-    return resp.json({error: `Prescription with name: ${req.params.name} not found.`});
+    return resp.json({ error: `Prescription with name: ${req.params.name} not found.` });
 });
 
-app.delete("/:name",(req,resp)=>{
+app.delete("/:name", (req, resp) => {
     const rxIndex = rxList.findIndex(c => c.name == req.params.name);
-    
+
     if (rxIndex) {
-        const deleteIndex=rxList.splice(rxIndex,1);
+        const deleteIndex = rxList.splice(rxIndex, 1);
         resp.status(200);
         return resp.json(deleteIndex);
     }
 
     resp.status(404);
-    return resp.json({error: `Prescription with name: ${req.params.name} not found.`});
+    return resp.json({ error: `Prescription with name: ${req.params.name} not found.` });
 });
 app.listen(port, () => {
     console.log(`Example express file server listening on port ${port}`);
